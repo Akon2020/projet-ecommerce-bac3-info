76f245a8c0d434d0dfb28651430fff152b51f3d5
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-05-13 16:16:23 +0200

    Initial Commit

---
+++ data.js
@@ -0,0 +1,182 @@
+export const data = [
+  {
+    id: 1,
+    img: "./assets/im1.png",
+    nom: "MacBook Pro Desktop",
+    prix: 2500,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Bureau",
+  },
+  {
+    id: 2,
+    img: "./assets/im2.png",
+    nom: "Casquette Microphone JBL T450 Sound Ecouteur",
+    prix: 80,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 3,
+    img: "./assets/im3.png",
+    nom: "Hard External drive 1TB 2.0",
+    prix: 100,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 4,
+    img: "./assets/im4.png",
+    nom: "Black modem router, Wireless router Wi-Fi",
+    prix: 85,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Reseau",
+  },
+  {
+    id: 5,
+    img: "./assets/im5.png",
+    nom: "Asus Red hat NVIDIA GeForce GTX 1080 Ti 11GB GDDR5X",
+    prix: 400,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Laptop",
+  },
+  {
+    id: 6,
+    img: "./assets/im6.png",
+    nom: "AirPods Pro 3",
+    prix: 35,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 7,
+    img: "./assets/im7.png",
+    nom: "Keyboard Gaming RGB LED Backlit Wired Keyboard",
+    prix: 90,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 8,
+    img: "./assets/im8.png",
+    nom: "Mouse LED Optical USB Mouse",
+    prix: 20,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 9,
+    img: "./assets/im9.png",
+    nom: "MacBook Air Laptop",
+    prix: 1500,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Laptop",
+  },
+  {
+    id: 10,
+    img: "./assets/im10.png",
+    nom: "Keyboard LogiTech Keyboard",
+    prix: 15,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 11,
+    img: "./assets/im11.png",
+    nom: "Mouse LogiTech Black Mouse",
+    prix: 10,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 12,
+    img: "./assets/im12.png",
+    nom: "HP i7 Intel Core Laptop 16GB RAM 1TB HDD 15.6 inch",
+    prix: 350,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Laptop",
+  },
+  {
+    id: 13,
+    img: "./assets/im13.png",
+    nom: "Wireless Router Wi-Fi IEEE",
+    prix: 79,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Reseau",
+  },
+  {
+    id: 14,
+    img: "./assets/im14.png",
+    nom: "HP i7 Grey Laptop 16GB RAM 1TB SSD",
+    prix: 368,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Laptop",
+  },
+  {
+    id: 15,
+    img: "./assets/im15.png",
+    nom: "PowerBank Mobile Charger",
+    prix: 10,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 16,
+    img: "./assets/im16.png",
+    nom: "MacBook Pro Xiaomi Mi Air",
+    prix: 500,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Laptop",
+  },
+  {
+    id: 17,
+    img: "./assets/im17.png",
+    nom: "Modem HUAWEI E5786 LTE Advanced MiFI 4G LTE",
+    prix: 55,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Reseau",
+  },
+  {
+    id: 18,
+    img: "./assets/im18.png",
+    nom: "Black JBL portable bluetooth speaker",
+    prix: 30,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+  {
+    id: 19,
+    img: "./assets/im19.png",
+    nom: "TECNO Camon 15 Pro 6GB RAM 128GB ROM",
+    prix: 190,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Communication",
+  },
+  {
+    id: 20,
+    img: "./assets/im20.png",
+    nom: "Microphone Yamaha Sound Input",
+    prix: 68,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempora veritatis dicta. Veritatis, laudantium aliquam. Beatae, ad? Fugiat provident ipsa dolores aut blanditiis illum pariatur, quod veritatis, harum dolore odio voluptatem fuga est voluptates laboriosam repellat ducimus hic quaerat. Accusamus repudiandae vitae beatae doloribus facere sunt distinctio soluta sed minus magni fuga, id hic fugiat aut dignissimos nam architecto, aperiam repellat, blanditiis officiis illum. Dolor quisquam quidem eius non impedit quo animi magni temporibus excepturi repudiandae voluptatibus in asperiores voluptas natus dolores quae, omnis exercitationem ipsum obcaecati ducimus laudantium! Esse, minima nobis dolorem nam velit illo dolore nemo, sequi explicabo rem, alias unde eaque. Quod, voluptatem.",
+    categorie: "Accessoire",
+  },
+];
